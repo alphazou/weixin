3751dec9cb44b0dbfcf629c2f0b76d306a38c4cb
alphazou <alphazou@qq.com>
2017-05-23 23:05:45 +0800

    5.23

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ orders = [None]
 
 @itchat.msg_register(['Text', 'Map', 'Card', 'Note', 'Sharing'])
 def text_reply(msg):
+    print(msg)
 
-    if msg['Text'] == '关机' and msg['FromUserName'] == '@a05d49bf468ce58b6c23fde2c767b71a':
+    if msg['Text'] == '关机' and msg['FromUserName'] == '@3701ce1817fa945377c79297aaf4246e':
         itchat.send('回复“是”确认关机！', msg['FromUserName'])
         orders[0] = '关机'
-    if (msg['Text'] == '是' and orders[0] == '关机') and msg['FromUserName'] == '@a05d49bf468ce58b6c23fde2c767b71a':
+    if (msg['Text'] == '是' and orders[0] == '关机') and msg['FromUserName'] == '@3701ce1817fa945377c79297aaf4246e':
         orders[0] = None
         itchat.send('即将关机！', msg['FromUserName'])
         # os.system('poweroff')
@@ -27,18 +28,18 @@ def download_files(msg):
     itchat.send('%s received' % msg['Type'], msg['FromUserName'])
     itchat.send('@%s@%s' % ('img' if msg['Type'] == 'Picture' else 'fil', fileDir), msg['FromUserName'])
 
-
-@itchat.msg_register('Friends')
-def add_friend(msg):
-    itchat.add_friend(**msg['Text'])
-    itchat.get_contract()
-    itchat.send('Nice to meet you!', msg['RecommendInfo']['UserName'])
-
-
-@itchat.msg_register('Text', isGroupChat=False)
-def text_reply(msg):
-    if msg['isAt']:
-        itchat.send(u'@%s\u2005I received: %s' % (msg['ActualNickName'], msg['Content']), msg['FromUserName'])
+#
+# @itchat.msg_register('Friends')
+# def add_friend(msg):
+#     itchat.add_friend(**msg['Text'])
+#     itchat.get_contract()
+#     itchat.send('Nice to meet you!', msg['RecommendInfo']['UserName'])
+#
+#
+# @itchat.msg_register('Text', isGroupChat=False)
+# def text_reply(msg):
+#     if msg['isAt']:
+#         itchat.send(u'@%s\u2005I received: %s' % (msg['ActualNickName'], msg['Content']), msg['FromUserName'])
 
 
 itchat.auto_login(hotReload=True)
